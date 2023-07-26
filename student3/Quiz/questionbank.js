7c974d6cb5f92468769960a0ae3313b42631bad7
Kavindu Sandaruwan <87182670+ImKavinduSandaruwan@users.noreply.github.com>
2023-07-26 07:26:26 +0530

    Add files via upload

---
+++ student3/Quiz/questionbank.js
@@ -0,0 +1,112 @@
+let question_bank = [
+    {
+    q_no: 1,
+    question: "Which National team won the latest FIFA world cup which was held in 2022 ? ",
+    answer: "Argentina",
+    options: [
+      "Brazil",
+      "France",
+      "Argentina",
+      "Uruguay"
+    ]
+  },
+    {
+    q_no: 2,
+    question: "What is the maximum number of players that are allowed on the field for each side ? ",
+    answer: "11",
+    options: [
+      "05",
+      "11",
+      "07",
+      "12"
+    ]
+  },
+    {
+    q_no: 3,
+    question: "Which football stadium is known to be the home of FC Barcelona ?",
+    answer: "Camp Nou ",
+    options: [
+      "Anfield ",
+      "Camp Nou ",
+      "Old Trafford ",
+      "Wembly "
+    ]
+  },
+    {
+    q_no: 4,
+    question: "Who is the player who's won the most Ballon d'Or awards ?",
+    answer: "Lionel Messi ",
+    options: [
+      "Raheem Sterling ",
+      "Erling Haaland ",
+      "Christiano Ronaldo ",
+      "Lionel Messi "
+    ]
+  },
+    {
+    q_no: 5,
+    question: "In which country was the 2018 FIFA World Cup held in ?",
+    answer: "Russia ",
+    options: [
+      "Qatar ",
+      "Japan ",
+      "Russia ",
+      "United States of America"
+    ]
+  },
+  {
+    q_no: 6,
+    question: "Which footballer has the highest transfer fee in history ?",
+    answer: "Neymar Jr. ",
+    options: [
+      "Neymar Jr. ",
+      "Lionel Messi ",
+      "Kylian Mbappe ",
+      "Thiago Silva "
+    ]
+  },
+  {
+    q_no: 7,
+    question: "In year was the first FIFA World Cup held ?",
+    answer: "1930 ",
+    options: [
+      "1948 ",
+      "1986 ",
+      "1930 ",
+      "2000 "
+    ]
+  },
+  {
+    q_no: 8,
+    question: "which club has won the most UEFA Champions League titles ? ",
+    answer: "Real Madrid ",
+    options: [
+      "Bayern Munich ",
+      "FC Barcelona ",
+      "Manchester United  ",
+      "Real Madrid "
+    ]
+  },
+  {
+    q_no: 9,
+    question: "For which club does Kylian Mbappe currently play ?",
+    answer: "Paris Saint Germain ",
+    options: [
+      "Paris Saint Germain ",
+      "Liverpool ",
+      "Real Madrid ",
+      "Manchester City"
+    ]
+  },
+  {
+    q_no: 10,
+    question: "To which club did Lionel Messi move to after leaving PSG ?",
+    answer: "Inter Miami FC ",
+    options: [
+      "FC Barcelona ",
+      "Inter Miami FC ",
+      "Al - Hilal ",
+      "Inter Milan"
+    ]
+  },
+];
---
+++ student3/Quiz/quiz.js
@@ -0,0 +1,184 @@
+//intializing variables
+const start_btn = document.querySelector(".start");
+const info_box = document.querySelector(".info_box");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
+const result_box = document.querySelector(".result_box");
+const option_list = document.querySelector(".option_list");
+const timerbar = document.querySelector("header .timerbar");
+const timeText = document.querySelector(".timer .time_left_txt");
+const timeCount = document.querySelector(".timer .timer_sec");
+
+// if start button clicked
+start_btn.onclick = ()=>{
+    info_box.classList.add("activeInfo"); //show info box
+}
+
+// if exit button clicked
+exit_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo"); //hide info box
+}
+
+// if continue button clicked
+continue_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo"); //hide info box
+    quiz_box.classList.add("activeQuiz"); //show quiz box
+    showQuetions(0); //calling showQestions function
+    queCounter(1); //passing 1 parameter to queCounter
+    startTimer(60); //calling startTimer function
+    startTimerLine(0); //calling startTimerLine function
+}
+
+let timeValue =  60;
+let que_count = 0;
+let que_numb = 1;
+let userScore = 0;
+let counter;
+let counterLine;
+let widthValue = 0;
+
+const restart_quiz = result_box.querySelector(".buttons .restart");
+const quit_quiz = result_box.querySelector(".buttons .quit");
+
+// if restart button clicked
+restart_quiz.onclick = ()=>{
+    quiz_box.classList.add("activeQuiz"); //show quiz box
+    result_box.classList.remove("activeResult"); //hide result box
+    timeValue = 60;
+    que_count = 0;
+    que_numb = 1;
+    userScore = 0;
+    widthValue = 0;
+    showQuetions(que_count); //calling showQestions function
+    queCounter(que_numb); //passing que_numb value to queCounter
+    clearInterval(counter); //clear counter
+    clearInterval(counterLine); //clear counterLine
+    startTimer(timeValue); //calling startTimer function
+    startTimerLine(widthValue); //calling startTimerLine function
+    timeText.textContent = "Time Left"; //change the text of timeText to Time Left
+    next_btn.classList.remove("show"); //hide the next button
+}
+
+// if quit button clicked
+quit_quiz.onclick = ()=>{
+    window.location.reload(); //reload the current window
+}
+
+const next_btn = document.querySelector("footer .next_btn");
+const bottom_ques_counter = document.querySelector("footer .total_que");
+
+// if Next button clicked
+next_btn.onclick = ()=>{
+    if(que_count < question_bank.length - 1){ //if question count is less than total question length
+        que_count++; //increment the que_count value
+        que_numb++; //increment the que_numb value
+        showQuetions(que_count); //calling showQestions function
+        queCounter(que_numb); //passing que_numb value to queCounter
+        
+        timeText.textContent = "Time Left"; //change the timeText to Time Left
+        next_btn.classList.remove("show"); //hide the next button
+    }else{
+        showResult(); //calling showResult function
+    }
+}
+
+// getting questions and options from array
+function showQuetions(index){
+    const que_text = document.querySelector(".que_text");
+
+    //creating a new span and div tag for question and option and passing the value using array index
+    let que_tag = '<span>'+ question_bank[index].q_no + ". " + question_bank[index].question +'</span>';
+    let option_tag = '<div class="option"><span>'+ question_bank[index].options[0] +'</span></div>'
+    + '<div class="option"><span>'+ question_bank[index].options[1] +'</span></div>'
+    + '<div class="option"><span>'+ question_bank[index].options[2] +'</span></div>'
+    + '<div class="option"><span>'+ question_bank[index].options[3] +'</span></div>';
+    que_text.innerHTML = que_tag; //adding new span tag inside que_tag
+    option_list.innerHTML = option_tag; //adding new div tag inside option_tag
+
+    const option = option_list.querySelectorAll(".option");
+
+    // set onclick attribute to all available options
+    for(i=0; i < option.length; i++){
+        option[i].setAttribute("onclick", "optionSelected(this)");
+    }
+}
+// creating the new div tags which for icons
+let tickIconTag = '<div class="icon tick"><i class="fas fa-check"></i></div>';
+let crossIconTag = '<div class="icon cross"><i class="fas fa-times"></i></div>';
+
+//if user clicked on option
+function optionSelected(answer){
+    let userAns = answer.textContent; //getting user selected option
+    let correcAns = question_bank[que_count].answer; //getting correct answer from array
+    const allOptions = option_list.children.length; //getting all option items
+
+    if(userAns == correcAns){ //if user selected option is equal to array's correct answer
+        userScore += 1; //upgrading score value with 1
+        answer.classList.add("correct"); //adding green color to correct selected option
+        answer.insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to correct selected option
+        console.log("Correct Answer");
+        console.log("Your correct answers = " + userScore);
+    }else{
+        answer.classList.add("incorrect"); //adding crimson color
+        answer.insertAdjacentHTML("beforeend", crossIconTag); //adding cross 
+        console.log("Wrong Answer");
+
+        for(i=0; i < allOptions; i++){
+            if(option_list.children[i].textContent == correcAns){ //if there is an option which is matched to an array answer
+                option_list.children[i].setAttribute("class", "option correct"); //adding theme color
+                option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag); //adding tick
+                console.log("Auto selected correct answer.");
+            }
+        }
+    }
+    for(i=0; i < allOptions; i++){
+        option_list.children[i].classList.add("disabled"); //once user select an option then disabled all options
+    }
+    next_btn.classList.add("show"); //show the next button if user selected any option
+}
+
+function showResult(){
+    info_box.classList.remove("activeInfo"); //hide info box
+    quiz_box.classList.remove("activeQuiz"); //hide quiz box
+    result_box.classList.add("activeResult"); //show result box
+    const scoreText = result_box.querySelector(".score_text");
+    scoreText.innerHTML = "You got "+userScore+" out of 10 answers correct";  //adding new span tag inside score_Text
+    
+}
+
+function startTimer(time){
+    counter = setInterval(timer, 1000);
+    function timer(){
+        timeCount.textContent = time; //changing the value of timeCount with time value
+        time--; //decrement the time value
+        if(time < 9){ //if timer is less than 9
+            let addZero = timeCount.textContent;
+            timeCount.textContent = "0" + addZero; //add a 0 before time value
+        }
+        if(time == 0){ //if timer is less than 0
+            clearInterval(counter); //clear counter
+            timeText.textContent = "Time Off"; //change the time text to time off
+            const allOptions = option_list.children.length; //getting all option items
+            let correcAns = question_bank[que_count].answer; //getting correct answer from array
+            showResult();
+        }
+    }
+}
+
+function startTimerLine(time){
+    counterLine = setInterval(timer, 29);
+    function timer(){
+        time += 0.263; //upgrading time value 
+        timerbar.style.width = time + "px"; //increasing width of timerbar with px by time value
+        if(time > 549){ //if time value is greater than 549
+            clearInterval(counterLine); //clear counterLine
+        }
+    }
+}
+
+function queCounter(index){
+    //creating a new span tag and passing the question number and total question
+    let totalQueCounTag = '<span><p>'+ index +'</p> of <p>'+ question_bank.length +'</p> Questions</span>';
+    bottom_ques_counter.innerHTML = totalQueCounTag;  //adding new span tag inside bottom_ques_counter
+}
